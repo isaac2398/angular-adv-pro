68f46344bd373d419cc708630a555025648fe212
Isaac Corona <coronaisaac@outlook.com>
2021-01-05 11:13:17 -0600

    Fin Seccion 3 - diseño

---
+++ src/app/pages/n404/n404.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-n404',
+  templateUrl: './n404.component.html',
+  styleUrls: ['./n404.component.css']
+  
+})
+export class N404Component  {
+
+  year:number = new Date().getFullYear();
+
+}
